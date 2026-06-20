9ef6e9b567110a39a09dca222d48339bfee5c049
Anupam Mishra <mishraanupammm004@gmail.com>
2026-06-20 14:34:24 +0530

    Added romantic app

--- test.py
+++ test.py
@@ -1,27 +1,13 @@
 import streamlit as st
 
-st.title("Anupam's AI App")
+st.title("❤️ For Someone Special")
 
-st.write("Hello Anupam! 🚀")
-import streamlit as st
-
-st.title("Anupam's First App")
+st.write("Welcome to Anupam's first deployed app!")
 
 name = st.text_input("Enter your name")
 
 if name:
-    st.write(f"Hello {name}! Welcome to Streamlit.")
-    import streamlit as st
-
-st.title("Anupam's AI Lab 🚀")
-
-st.write("Python + Streamlit + GitHub are connected successfully!")
-
-if st.button("Click Me"):
-    st.success("Everything is working perfectly!")
-    import streamlit as st
-
-st.title("❤️ For Someone Special")
+    st.write(f"Hello {name}! 😊")
 
 if st.button("Click Me"):
     st.balloons()
