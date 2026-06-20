bf9de46086ccb658be5c644e1b0c1290092cbc4f
Anupam Mishra <mishraanupammm004@gmail.com>
2026-06-20 10:27:05 +0530

    First Streamlit app

---
+++ test.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("My First Web App")
+
+st.write("Hello Anupam! 🚀")
+import streamlit as st
+
+st.title("Anupam's First App")
+
+name = st.text_input("Enter your name")
+
+if name:
+    st.write(f"Hello {name}! Welcome to Streamlit.")
+    
