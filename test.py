5c5778abdee13922347083bc5f79793942bb7f3d
Anupam Mishra <mishraanupammm004@gmail.com>
2026-06-20 14:13:43 +0530

    Connectivity test

--- test.py
+++ test.py
@@ -11,4 +11,11 @@ name = st.text_input("Enter your name")
 
 if name:
     st.write(f"Hello {name}! Welcome to Streamlit.")
-    
+    import streamlit as st
+
+st.title("Anupam's AI Lab 🚀")
+
+st.write("Python + Streamlit + GitHub are connected successfully!")
+
+if st.button("Click Me"):
+    st.success("Everything is working perfectly!")
